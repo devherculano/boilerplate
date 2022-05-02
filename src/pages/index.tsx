7480c31af924a9073b1252b54a0999a4527b8f0d
Tsherculano <tico.herculano@gmail.com>
2022-05-02 08:17:18 -0300

    feat: Create branch dev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
 
-type Props = {
-  title: string;
-};
-
-export default function Home({ title = 'React Avançado!' }: Props) {
+export default function Home({ title = 'React Avançado!' }) {
   return (
     <div className={styles.container}>
       <Head>
